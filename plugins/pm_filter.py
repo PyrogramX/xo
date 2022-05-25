331189d0b1128bf0bdc0eba0c1422b5524a1c419
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-05-25 14:46:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -769,22 +769,20 @@ async def advantage_spell_chok(msg):
         await k.delete()
         return
     SPELL_CHECK[msg.message_id] = movielist
-    #btn = [[
-     #           InlineKeyboardButton(
-     #               text=f"How To Download ❓",
-     #               url=f"https://t.me/howtowatchlink",
-     #           ),
-     #           InlineKeyboardButton(
-     #               text=f"Help or RequestMovie",
-     #               url=f"https://t.me/hdmoviestudioinquiry",
-     #           ),]
-             [for k, movie in enumerate(movielist)]
-    btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything\nPlease send correct movie spelling❗👉🏻<a href='https://t.me/howtowatchlink'>How To Download ❓</a> Or <a href='https://t.me/hdmoviestudioinquiry'>Contact Help or Request Movie</a>",
-                    reply_markup=InlineKeyboardMarkup(btn))
-    #await asyncio.sleep(8)
-    #await msg.delete()
-    #return
+    btn = [[
+                InlineKeyboardButton(
+                    text=f"How To Download ❓",
+                    url=f"https://t.me/howtowatchlink",
+                ),
+                InlineKeyboardButton(
+                    text=f"Help or RequestMovie",
+                    url=f"https://t.me/hdmoviestudioinquiry",
+                ),
+             ] for k, movie in enumerate(movielist)]
+    #btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
+    await msg.reply("I couldn't find anything\nPlease send correct movie spelling❗👉🏻<a href='https://t.me/howtowatchlink'>How To Download ❓</a> Or <a href='https://t.me/hdmoviestudioinquiry'>Contact Help or Request Movie</a>",) 
+                    #reply_markup=InlineKeyboardMarkup(btn))
+    
 
 op = [[
                 InlineKeyboardButton(
